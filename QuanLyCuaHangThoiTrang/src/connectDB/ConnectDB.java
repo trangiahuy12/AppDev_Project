01334f6bd28e6197ad01f5bb9a826e9627749316
hoangnguyen1903 <hoangnguyen.19032003@gmail.com>
2023-10-27 21:33:55 +0700

    Tao giao dien BanHang, ThongTinHoaDon, DoiTra, ThongTinDoiTra, ThongTinHoanTra

---
+++ QuanLyCuaHangThoiTrang/src/connectDB/ConnectDB.java
@@ -0,0 +1,36 @@
+package connectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+	public static Connection con = null;
+	private static ConnectDB instance = new ConnectDB();
+	
+	public static ConnectDB getInstance() {
+		return instance;
+	}
+	
+	public void connect() throws SQLException {
+		String url = "jdbc:sqlserver://localhost:1433;databasename=QuanLyThoiTrang";
+		String user = "sa";
+		String password = "";
+		con = DriverManager.getConnection(url, user, password);
+	}
+	
+	public void disconnect() {
+		if(con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static Connection getConnection() {
+		return con;
+	}
+}
